06781059e66984951e176f7cbe9d32cd3b5ffa5d
chenboheng41 <cbh15736780172@sina.com>
2019-04-18 17:16:43 +0800

    提交信息

--- index.js
+++ index.js
@@ -7,8 +7,10 @@ const productRouter=require('./routes/proguct.js');
 const userRouter=require('./routes/user.js');
 //创建web服务器
 var server=express();
-server.listen(5050);
-//使用body-parser中间件
+server.listen(5050,()=>{
+    console.log("服务器创建成功")
+});
+//使用body-parser中间件 
 server.use(bodyParser.urlencoded({extended:false}));
 //托管静态资源到public
 server.use(express.static('./product'));
